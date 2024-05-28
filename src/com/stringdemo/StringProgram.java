820536a2471ba3dd639d2bebe7c1f46fc1ccd6d5
Maneesh <mk049076@gmail.com>
2024-05-28 16:21:47 +0530

    [Maneesh ]Group of anagram and other programs

--- src/com/stringdemo/StringProgram.java
+++ src/com/stringdemo/StringProgram.java
@@ -2,10 +2,13 @@ package com.stringdemo;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.Stack;
 import java.util.stream.Collectors;
 
 public class StringProgram {
@@ -77,8 +80,144 @@ public class StringProgram {
         }).reduce("",(ans,i)->ans+" "+i);
         System.out.println(revEveWord3);
         
+        System.out.println("9) Remove Duplicate character from an string");
+        s="Hi my name is Maneesh";
+        System.out.println(removeDuplicates(s));
+        
+        System.out.println("Check one string is rotating of other or not");
+        s1="maneesh"; s2="aneeshm";
+        System.out.println(checkRotating(s1,s2));
+        
+        System.out.println("Sort the chracter in dtring by their freq");
+        s="aabbcddd";
+        //out caabbddd
+        System.out.println(sortByCharacter(s));
+        
+        
+        System.out.println("Balance parnthesis check: ");
+        s="[()]{}{[()()]()}";
+//       exp = “[()]{}{[()()]()}” 
+//        		Output: Balanced
+//        		Explanation: all the brackets are well-formed
+//
+//        		Input: exp = “[(])” 
+//        		Output: Not Balanced 
+//        		Explanation: 1 and 4 brackets are not balanced because 
+//        		there is a closing ‘]’ before the closing ‘(‘
+        if (checkBalance(s))
+        System.out.println("Balanced");
+        else System.out.println(" Not Balanced");
+        
+        
+        System.out.println(" Group of anagram: "
+        		+ "");
+        ArrayList<String> list = new ArrayList<>();
+        list.add("cat");
+        list.add("dog");
+        list.add("ogd");
+        list.add("god");
+        list.add("atc");
+ 
+        System.out.println(groupOfAnagram(list));
+        
 	}
 	
+	
+	
+	
+	
+	public static List<String> groupOfAnagram(List<String> list){
+		HashMap<HashMap<Character, Integer>, ArrayList<String> > map =new HashMap<>();
+		for (Iterator iterator = list.iterator(); iterator.hasNext();) {
+			String s = (String) iterator.next();
+			HashMap<Character , Integer> tempMap=new HashMap<>();
+			for (int i = 0; i < s.length(); i++) {
+				tempMap.put(s.charAt(i), tempMap.getOrDefault(s.charAt(i), 0)+1);
+			}
+			if(map.containsKey(tempMap)) {
+				map.get(tempMap).add(s);
+			}
+			else {
+				ArrayList<String > tempList=new ArrayList<>();
+				tempList.add(s);
+				map.put(tempMap, tempList);
+			}
+			
+		}
+		ArrayList <String > res=new ArrayList<>();
+		for (Map.Entry<HashMap<Character, Integer>, ArrayList<String>> entry : map.entrySet()) {
+			HashMap<Character, Integer> key = entry.getKey();
+			ArrayList<String> val = entry.getValue();
+			res.addAll(val);
+			
+			
+		}
+		return res;
+	}
+	
+	public static boolean checkBalance(String x) {
+		 Stack<Character> st = new Stack<>();
+			char[] charArray = x.toCharArray();
+			for (char c : charArray) {
+				if(c=='{' || c=='[' || c=='(') st.push(c);
+				else {
+					if (c=='}' && !st.isEmpty() && st.peek()=='{')  st.pop();
+					else if (c==']' && !st.isEmpty() && st.peek()=='[')  st.pop();
+					else if (c==')' && !st.isEmpty() && st.peek()=='(')  st.pop();
+					else if((c==']' || c=='}' || c==')') && st.isEmpty()) return false;
+					else if(c=='}' && (st.peek()=='[' || st.peek()=='(')) return false;
+					else if(c==']' && (st.peek()=='{' || st.peek()=='(')) return false;
+					else if(c==')' && (st.peek()=='[' || st.peek()=='{')) return false;
+				}
+			}
+			if(st.isEmpty()) return true;
+			
+			return false;
+	}
+	public static String sortByCharacter(String s) {
+		Map<Character, Integer> map= new HashMap<>();
+		for (Character ch : s.toCharArray()) {
+			map.put(ch, map.getOrDefault(ch, 0)+1);
+			
+			
+		}
+		List<Map.Entry<Character , Integer>> entryList= new ArrayList<>(map.entrySet());
+		Collections.sort(entryList, (e1,e2)->e1.getValue().compareTo(e2.getValue()));
+		StringBuilder res=new StringBuilder();
+		for (Map.Entry<Character, Integer> entry : entryList) {
+			for (int i = 0; i < entry.getValue(); i++) {
+				res.append(entry.getKey());
+			}
+		}
+		return res.toString();
+	}
+	
+	public static boolean checkRotating(String s1, String s2) {
+		if(s1.equals(s2)) return true;
+		for (int i = 0; i < s1.length(); i++) {
+			String temp=s1.substring(i+1)+s1.substring(0,i+1);
+			if(temp.equals(s2)) return true;
+		}
+		return false;
+	}
+	
+	
+	private static String removeDuplicates(String s) {
+		// TODO Auto-generated method stub
+		Map<Character, Integer> map= new HashMap<>();
+		String res ="";
+		for (int i = 0; i < s.length(); i++) {
+			if(map.containsKey(s.charAt(i)) && s.charAt(i)!=' ') continue;
+			else {
+				res=res+s.charAt(i);
+				map.put(s.charAt(i), 1);
+			}
+		}
+		return res;
+	}
+
+
+
 	public static String reverse(String s) {
 		StringBuilder sb= new StringBuilder(s);
 		sb.reverse();
